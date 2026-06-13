c36636a713c35f71ba0ddc56c0f62754ba5d075c
SnehaM-96 <snehamaudshetty2006@gmail.com>
2026-06-13 21:28:31 +0530

    Initial commit - Streamlit RAG app with Gemini integration

--- rag/qa_engine.py
+++ rag/qa_engine.py
@@ -1,13 +1,12 @@
-from dotenv import load_dotenv
+import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
-
-load_dotenv()
 
 def get_llm():
 
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
-        temperature=0.3
+        model="gemini-1.5-flash",
+        temperature=0.3,
+        google_api_key=st.secrets["GOOGLE_API_KEY"]
     )
 
     return llm
