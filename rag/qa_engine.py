0cd73d3fa5cc59da03b264d3e061e80e74b137d0
SnehaM-96 <snehamaudshetty2006@gmail.com>
2026-06-13 22:15:34 +0530

    fix dependencies

--- rag/qa_engine.py
+++ rag/qa_engine.py
@@ -1,12 +1,9 @@
-import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st
 
 def get_llm():
-
-    llm = ChatGoogleGenerativeAI(
+    return ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",
         temperature=0.3,
         google_api_key=st.secrets["GOOGLE_API_KEY"]
     )
-
-    return llm
